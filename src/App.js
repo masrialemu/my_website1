cad21d392e3d63da694510a636786a56b9121741
Masri Alemu <masrialemu@gmail.com>
2023-11-04 22:31:21 +0300

    v1.3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Redirect  } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import Home from './Main/Home';
 import NotFound from './Main/NotFound';
 import LoginMain from './Component/Login/LoginMain';
@@ -9,28 +9,18 @@ import DetailMain from './Component/Body/DetailMain';
 function App() {
   const userData = JSON.parse(localStorage.getItem("userData"));
   const token = userData ? userData.token : null;
+
   return (
     <div>
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/home">
-          <Home />
-        </Route>
-        <Route path="/login">
-          <LoginMain />
-        </Route>
-        <Route path="/post">
-            {token ? <PostMainn /> : <Redirect to="/login" />}
-        </Route>
-        <Route path="/project/:postid">
-          <DetailMain />
-        </Route>
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/home" component={Home} />
+      <Route path="/login" component={LoginMain} />
+      <Route
+        path="/post"
+        render={() => (token ? <PostMainn /> : <Redirect to="/login" />)}
+      />
+      <Route path="/project/:postid" component={DetailMain} />
+      <Route component={NotFound} />
     </div>
   );
 }
