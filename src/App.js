ef766a88336a1f40f62166f713af9790049bbade
Masri Alemu <masrialemu@gmail.com>
2023-11-05 00:16:58 +0300

    v1.22

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import Home from './Main/Home';
 import NotFound from './Main/NotFound';
 import LoginMain from './Component/Login/LoginMain';
@@ -12,15 +12,17 @@ function App() {
 
   return (
     <div>
-      <Route path="/" exact component={Home} />
-      <Route path="/home" component={Home} />
-      <Route path="/login" component={LoginMain} />
-      <Route
-        path="/post"
-        render={() => (token ? <PostMainn /> : <Redirect to="/login" />)}
-      />
-      <Route path="/project/:postid" component={DetailMain} />
-      <Route path="*" component={NotFound} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/home" component={Home} />
+        <Route path="/login" component={LoginMain} />
+        <Route
+          path="/post"
+          render={() => (token ? <PostMainn /> : <Redirect to="/login" />)}
+        />
+        <Route path="/project/:postid" component={DetailMain} />
+        <Route path="*" component={NotFound} />
+      </Switch>
     </div>
   );
 }
